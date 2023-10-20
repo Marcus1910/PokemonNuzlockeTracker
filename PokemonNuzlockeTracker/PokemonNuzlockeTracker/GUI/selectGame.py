db364987d253d3ac1b9440ceaf305a9e2d135574
Marcus1910 <54845611+Marcus1910@users.noreply.github.com>
2023-10-20 18:34:52 +0200

    added background

--- PokemonNuzlockeTracker/PokemonNuzlockeTracker/GUI/selectGame.py
+++ PokemonNuzlockeTracker/PokemonNuzlockeTracker/GUI/selectGame.py
@@ -1,10 +1,12 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 import games as gm
+import os
 
 #define different screens
 class TrainerScreen(Screen):
@@ -23,11 +25,18 @@ class WindowManager(ScreenManager):
 
 class SelectGame(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical')
-        label = Label(text=f"{gm.checkGames()}")
-        layout.add_widget(label)
+        root = BoxLayout()
+        # pathToImage = os.path.join(os.path.dirname(os.getcwd()), "images", "bg.jpg")
+        # print(pathToImage)
+        bgImage = Image(source = os.path.join(os.path.dirname(os.getcwd()), "images", "bg.jpg"))
+        bgImage.allow_stretch = True
+        bgImage.keep_ratio = True
+        root.add_widget(bgImage)
+        # layout = BoxLayout(orientation='vertical')
+        # label = Label(text=f"{gm.checkGames()}")
+        # layout.add_widget(label)
 
-        return layout
+        return root
         # self.sm = WindowManager()
         # screen1 = TrainerScreen()
         # screen2 = ItemScreen()
