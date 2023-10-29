c2258ea8ace81441942114b37db96e07a4492d28
Marcus1910 <54845611+Marcus1910@users.noreply.github.com>
2023-10-29 15:44:16 +0100

    hehe

--- PokemonNuzlockeTracker/PokemonNuzlockeTracker/GUI/trainerScreen.py
+++ PokemonNuzlockeTracker/PokemonNuzlockeTracker/GUI/trainerScreen.py
@@ -64,7 +64,7 @@ class TrainerScreen(NuzlockeScreen):
             #create Image with name underneath
             imageBox = BoxLayout(orientation = "vertical", size_hint_x = 0.3)
             #imgButton = Button(text = "image", size_hint_y = 0.7, pos_hint = {"top" : 1})
-            imgButton = Image(source = os.path.join(self.spriteFolder, f"{pokemonObject.name}.png"))
+            imgButton = Image(source = os.path.join(self.spriteFolder, f"{pokemonObject.name.lower()}.png"))
             
             nameLevelBox = BoxLayout(orientation = "horizontal", size_hint_y = 0.3, pos_hint = {"bottom" : 1})
             nameInput = TextInput(text = f"{pokemonObject.name}", background_normal="", background_color=(1, 1, 1, 0.5), size_hint_x = 0.6)
