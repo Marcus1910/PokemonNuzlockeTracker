805e8cce9e9978028ecce9141e26f14b1c0b17f7
MelkMan2001 <54845611+MelkMan2001@users.noreply.github.com>
2024-05-12 18:56:57 +0200

    added filehandler to logiclogger so debug level gets send to file

--- loggerConfig.py
+++ loggerConfig.py
@@ -1,25 +1,28 @@
 import logging
 open("trackerLogic.log", "w").close()
+
+formatter = logging.Formatter('%(filename)s - %(lineno)s - %(levelname)s - %(message)s')
 # Get the logger for your application
 logger = logging.getLogger('PokemonNuzlockeTracker')
-logicLogger = logging.getLogger('PokemonNuzlockeTracker logic')
 
 logger.setLevel(logging.DEBUG)
-logicLogger.setLevel(logging.INFO)
-
 logger.propagate = False
-logicLogger.propagate = False
+loggerHandler = logging.StreamHandler()
+loggerHandler.setLevel(logging.DEBUG)
+loggerHandler.setFormatter(formatter)
+logger.addHandler(loggerHandler)
 
-handler = logging.StreamHandler()
+logicLogger = logging.getLogger('PokemonNuzlockeTracker logic')
+logicLogger.setLevel(logging.DEBUG)
+logicLogger.propagate = False
+logicHandler = logging.StreamHandler()
+logicHandler.setLevel(logging.INFO)
+logicHandler.setFormatter(formatter)
+logicLogger.addHandler(logicHandler)
 
 fileHandler= logging.FileHandler("trackerLogic.log")
 fileHandler.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(filename)s - %(lineno)s - %(levelname)s - %(message)s')
 fileHandler.setFormatter(formatter)
-handler.setFormatter(formatter)
-
-logger.addHandler(handler)
-logicLogger.addHandler(handler)
 logicLogger.addHandler(fileHandler)
+
 
